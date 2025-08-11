047627924d274d842697a998099254f17f73d533
KellyYexabel <165528555+KellyYexabel@users.noreply.github.com>
2025-08-10 21:36:30 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import com.bys.gui.VentanaPrincipal;
 
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         GestorProducto gestorProducto = new GestorProducto();
